7f9fbe7b5fc5ce7d49846d8746c335587920a43c
sruthilaya <sruthilayagadila@gmail.com>
2024-01-08 19:23:42 +0530

    mysql

---
+++ main.tf
@@ -0,0 +1,57 @@
+resource "aws_docdb_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = var.subnet_ids
+
+  tags =  merge({
+    Name = "${var.component}-${var.env}"
+  }),
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id = var.vpc_id
+
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${var.component}-${var.env}-cluster"
+  engine                  =  var.engine
+  engine_version          = var.engine_version
+  master_username         = data.aws_ssm_parameter.username.value
+  master_password         = data.aws_ssm_parameter.password.value
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.main.id]
+  kms_key_id              = var.kms_key_arn
+  db_subnet_group_name    = aws_docdb_subnet_group.main.name
+  storage_encrypted       = true
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = length(var.instance_count)
+  identifier         = "${var.component}-${var.env}-${count.index}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  engine =  aws_docdb_cluster.docdb.engine
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "component" {}
+variable "env" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_subnet_cidr" {}
+variable "port" {
+  default = 27017
+}
+variable "engine" {}
+variable "instance_class" {}
+variable "instance_count" {}
+variable "engine_version" {}
+variable "kms_key_arn" {}
